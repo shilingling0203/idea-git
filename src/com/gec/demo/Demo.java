7542b3deda35c42b01117b26f952948e056c011c
shilingling0203 <shiling@qq.com>
2021-05-12 11:19:15 +0800

    第二次提交

--- src/com/gec/demo/Demo.java
+++ src/com/gec/demo/Demo.java
@@ -4,6 +4,6 @@ public class Demo {
     public static void main(String[] args) {
         System.out.println("你好!!!");
         System.out.println("Hello");
-        System.out.println("石玲玲来了");
+        System.out.println("石玲玲来了生生世世");
     }
 }
